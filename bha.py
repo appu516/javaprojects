41900c86ba3ec5500ea67bf34f21ed80ab998d66
appu516 <hbharath516@gmail.com>
2025-07-19 19:03:45 +0530

    Add files via upload

---
+++ bha.py
@@ -0,0 +1,21 @@
+import turtle
+def draw_multicolor_square(animal,size):
+    """Make animal draw a multi-color square of given size."""
+    for color in ["red","purple","hotpink","blue"]:
+        animal.color(color)
+        animal.forward(size)
+        animal.left(90)
+        
+    window=turtle.screen()
+    window.bgcolor("lightgreen")
+    
+    tess=turtle.Turtle()
+    tess.pensize(3)
+    
+    size=20
+    for i in range(15):
+        draw_multicolor_square(tess,size)
+        size +10
+        tess.forward(10)
+        tess.right(18)
+        window.mainloop()
